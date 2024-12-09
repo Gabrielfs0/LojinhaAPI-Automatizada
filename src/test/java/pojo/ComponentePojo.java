8c97daf9419997c495b7dd1773a7980e6a23686d
Gabrielfs0 <gf12166@gmail.com>
2024-12-09 20:01:17 -0300

    first commit

---
+++ src/test/java/pojo/ComponentePojo.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class ComponentePojo {
+    private String nomeComponente;
+    private int quantidadeComponente;
+
+    public String getNomeComponente() {
+        return nomeComponente;
+    }
+
+    public void setNomeComponente(String nomeComponente) {
+        this.nomeComponente = nomeComponente;
+    }
+
+    public int getQuantidadeComponente() {
+        return quantidadeComponente;
+    }
+
+    public void setQuantidadeComponente(int quantidadeComponente) {
+        this.quantidadeComponente = quantidadeComponente;
+    }
+}
